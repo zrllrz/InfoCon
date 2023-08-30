fix loss average method bugs

src/autocot.py
@@ -94,7 +94,7 @@ class ExplicitSAGPTConfig(RootConfig):
 class ExplicitSAHNGPTConfig(RootConfig):
     def __init__(self, n_embd, n_head,
                  attn_pdrop, resid_pdrop, embd_pdrop,
-                 block_size, n_layer, n_state_layer, use_skip, max_timestep):
+                 block_size, n_layer, n_state_layer, use_skip, max_timestep, use_future_state=False):
         self.type = 'egpthn'
         super().__init__(
             n_embd, n_head,
@@ -103,6 +103,7 @@ class ExplicitSAHNGPTConfig(RootConfig):
         )
         self.n_state_layer = n_state_layer
         self.use_skip = use_skip
+        self.use_future_state = use_future_state
 
 
 class ENetConfig(RootConfig):
@@ -170,7 +171,9 @@ class AutoCoT(pl.LightningModule):
         e_dim=-1,
         vq_t_emb_rate=2.0,
         vq_coe_r_l1=0.0,
-        vq_use_clip_decrease_r=False
+        vq_use_prob_sel_train=False,
+        vq_use_timestep_appeal=False,
+        task=''
     ):
         super().__init__()
 
@@ -289,7 +292,8 @@ class AutoCoT(pl.LightningModule):
             use_ema=True,
             coe_ema=vq_coe_ema,
             t_emb_rate=vq_t_emb_rate,
-            use_clip_decrease_r=vq_use_clip_decrease_r
+            use_prob_sel_train=vq_use_prob_sel_train,
+            use_timestep_appeal=vq_use_timestep_appeal
         )
         self.vq_coe_r_l1 = vq_coe_r_l1
 
@@ -307,9 +311,11 @@ class AutoCoT(pl.LightningModule):
         if optimizers_config is not None:
             self.coe_cluster = optimizers_config['coe_cluster']
             self.coe_rec = optimizers_config['coe_rec']
+            self.use_decay_mask_rate = False if 'use_decay_mask_rate' not in optimizers_config.keys() else optimizers_config['use_decay_mask_rate']
         else:
             self.coe_cluster = 0.0
             self.coe_rec = 0.0
+            self.use_decay_mask_rate = False
 
         self.step = 0
         self.step_cluster = 0
@@ -320,6 +326,7 @@ class AutoCoT(pl.LightningModule):
         self.progress_bar_step = 0.0
 
         self.automatic_optimization = False
+        self.mytask = task
 
     def mask_cluster_rate(self):
         # calculate the mask off rate of cluster according to action prediction behavior
@@ -341,6 +348,7 @@ class AutoCoT(pl.LightningModule):
 
     def statistic_indices(self, indices, unified_t=None):
         # (B, T)
+        print(self.mytask)
         if unified_t is None:
             table = torch.zeros(size=(indices.shape[0], self.key_book.n_e))  # (B, n_e)
             for i in range(self.key_book.n_e):
@@ -356,10 +364,8 @@ class AutoCoT(pl.LightningModule):
                     table[i, j] = ((indices == j).to(torch.int) * mask_t).sum()
             torch.set_printoptions(precision=8, sci_mode=False)
             print(table)
-            print(torch.div(F.sigmoid(self.key_book.t_keys.weight.data), self.key_book.n_e).squeeze(-1)
-                  + self.key_book.t_base.squeeze(-1))
+            print(self.key_book.t_keys.weight.data.squeeze(-1))
             print(self.key_book.get_r().squeeze(-1))
-            print(self.key_book.r_p_reset)
 
     def loss_reward(self, states, indices):
         # indices (B, T) choice of index of this batch
@@ -368,17 +374,8 @@ class AutoCoT(pl.LightningModule):
         assert self.sa_type == 'egpt' or self.sa_type == 'egpthn'
         for di in range(self.key_book.n_e):
             self.vq_turn = ((self.vq_turn + 1) % self.key_book.n_e)
-            # print('indices', indices)
-            # print('self.vq_turn', self.vq_turn)
-            # input()
-            # print('indices', indices)
-            # print('indices.shape', indices.shape)
-            # input()
             mask_i = torch.eq(indices, self.vq_turn)
-            # print(mask_i)
-            # input()
             n_i = mask_i.sum()
-            # print('n_i', n_i)
             if n_i != 0:
                 # update the reward function of this part
                 key_same_i = self.key_book.vparams(torch.tensor([[self.vq_turn]], device=states.device))
@@ -387,7 +384,6 @@ class AutoCoT(pl.LightningModule):
                 reward_i, _ = self.sa_net.get_reward(states, key_same_i)
                 reward_i = torch.where(mask_i, reward_i, 1.0 - reward_i)  # other should be different
                 return reward_i
-                # return torch.zeros_like(indices, dtype=torch.float32, device=states.device)
 
         print('in autocot method: loss_reward: should not reach here!')
         assert False
@@ -422,28 +418,16 @@ class AutoCoT(pl.LightningModule):
         # first forward, use policy and implicit energy to adjust key_soft, key_book #
         ##############################################################################
         key_soft, state_trans = self.key_net(states, timesteps, actions)
-        encoding_indices, key_hard, vparams_w, vparams_hard, w_max, score_ksh, key_soft_t_emb = \
+        encoding_indices, key_hard, vparams_w, vparams_hard, w_max, w_cnt, key_soft_t_emb = \
             self.key_book(key_soft, unified_t)
 
         if self.future_net is not None:
-            # keys_norm = self.key_book.get_keys() * self.key_book.get_r()
-            # keys_norm = \
-            #     keys_norm[encoding_indices.view(-1)].view(encoding_indices.shape[0], encoding_indices.shape[1], -1)
-            # # don't forget to store gradient!
-            # keys_norm = \
-            #     F.normalize(key_soft, p=2.0, dim=-1) \
-            #     + (keys_norm - F.normalize(key_soft, p=2.0, dim=-1)).detach()
             state_future_preds = self.future_net(states, timesteps, actions, keys=key_hard)
             state_future = self.get_future_state(states, encoding_indices)
             l_future, _ = get_loss(state_future_preds, state_future, lengths)
         else:
             l_future = 0.0
-        # print('just after book')
-        # input()
-        # print('encoding_indices', encoding_indices)
-        # print('encoding_indices.shape', encoding_indices.shape)
-        # print('max, min', encoding_indices.max(), encoding_indices.min())
-        # input()
+            state_future = None
         state_recs_from_keys = self.rec_net(key_soft_t_emb, timesteps)
 
         # loss: state transmission
@@ -453,6 +437,7 @@ class AutoCoT(pl.LightningModule):
 
         # loss: clustering behind
         enough_mask = torch.less(w_max, 0.5).to(dtype=torch.float32)
+        # print('w_cnt.shape', w_cnt.shape)
         ls_label_cluster = torch.neg(torch.log(w_max))
         l_label_cluster = ls_label_cluster.mean()
 
@@ -473,25 +458,25 @@ class AutoCoT(pl.LightningModule):
             l_policy = l_a_preds + l_s_trans + self.coe_rec * l_s_recs
 
         elif self.sa_type == 'egpt' or self.sa_type == 'egpthn':
-            action_preds, reward, v_r_norm = self.sa_net(states, timesteps, actions=actions, keys=vparams_hard)
+            action_preds, reward, v_r_norm = self.sa_net(states, timesteps, actions=actions, keys=vparams_hard, future_states=state_future)
 
             # loss: state prediction & action prediction
             l_s_preds = 0.0
             l_a_preds, ls_a_preds = get_loss(action_preds, actions, lengths)
 
             # select a part of action prediction, only update a part of clustering related loss according to it
-            ls_a_preds_flattened = ls_a_preds.view(-1)
-            mask_cluster_rate = self.mask_cluster_rate()
-            _, cluster_mask_index = \
-                torch.topk(ls_a_preds_flattened, k=int(mask_cluster_rate*ls_a_preds_flattened.shape[0]))
-            cluster_mask_flattened = torch.ones_like(ls_a_preds_flattened, dtype=torch.float32)
-            # print('cluster_mask_flattened.shape', cluster_mask_flattened.shape)
-            # input()
-            # print('cluster_mask_index.shape', cluster_mask_index.shape)
-            # input()
-            cluster_mask_flattened[cluster_mask_index] = 0.0
+            if self.use_decay_mask_rate:
+                ls_a_preds_flattened = ls_a_preds.view(-1)
+                mask_cluster_rate = self.mask_cluster_rate()
+                _, cluster_mask_index = \
+                    torch.topk(ls_a_preds_flattened, k=int(mask_cluster_rate*ls_a_preds_flattened.shape[0]))
+                cluster_mask_flattened = torch.ones_like(ls_a_preds_flattened, dtype=torch.float32)
+                cluster_mask_flattened[cluster_mask_index] = 0.0
+                cluster_mask = cluster_mask_flattened.reshape(ls_a_preds.shape)
+            else:
+                mask_cluster_rate = 0.0
+                cluster_mask = None
 
-            cluster_mask = cluster_mask_flattened.reshape(ls_a_preds.shape)
             self.log(name='cm', value=mask_cluster_rate, prog_bar=True, on_step=True, on_epoch=True)
             # print('rate mask', cluster_mask.sum().item() / ls_a_preds_flattened.shape[0])
             # print('cluster_mask.shape', cluster_mask.shape)
@@ -513,14 +498,28 @@ class AutoCoT(pl.LightningModule):
                 # print('encoding_indices', encoding_indices)
                 reward_i = self.loss_reward(states, indices=encoding_indices)
                 self.log(name='rt', value=float(self.vq_turn), prog_bar=True, on_step=True, on_epoch=True)
-                # print('after reward_i')
                 reg_r_l2 = (self.key_book.get_r() ** 2).sum()
+
+                ls_label_cluster_masked = ls_label_cluster * enough_mask
+                if self.use_decay_mask_rate:
+                    print('use_decay_mask_rate')
+                    ls_label_cluster_masked = ls_label_cluster_masked * cluster_mask
+                ls_label_cluster_weighed = ls_label_cluster_masked * w_cnt
+
+                ls_reward = torch.neg(torch.log(1.0 - reward))
+                ls_reward_i = torch.neg(torch.log(1.0 - reward_i))
+                if self.use_decay_mask_rate:
+                    print('use_decay_mask_rate')
+                    ls_reward = ls_reward * cluster_mask
+                    ls_reward_i = ls_reward_i * cluster_mask
+                ls_reward_weighed = ls_reward * w_cnt
+
                 l_policy = \
                     l_policy \
                     + 2.0 * self.coe_cluster * (
-                        (ls_label_cluster * cluster_mask * enough_mask).mean()
-                        + (torch.neg(torch.log(1.0 - reward)) * cluster_mask).mean()
-                        + (torch.neg(torch.log(1.0 - reward_i)) * cluster_mask).mean()
+                        torch.div(ls_label_cluster_weighed.sum(), self.key_book.n_e)
+                        + torch.div(ls_reward_weighed.sum(), self.key_book.n_e)
+                        + ls_reward_i.mean()
                         + self.vq_coe_r_l1 * reg_r_l2
                     )
 

src/module/GPT.py
@@ -457,7 +457,11 @@ class ExplicitSAHNGPT(nn.Module):
 
         # State embeddings & Action embeddings & Key embeddings
         self.state_encoder = MLP(state_dim, config.n_embd, hidden_dims=[256])
-        self.state_grad_encoder = MLP(state_dim, config.n_embd, hidden_dims=[256])
+        self.use_future_state = config.use_future_state
+        if config.use_future_state:
+            self.state_grad_encoder = MLP(state_dim * 2, config.n_embd, hidden_dims=[256])
+        else:
+            self.state_grad_encoder = MLP(state_dim, config.n_embd, hidden_dims=[256])
         self.action_encoder = MLP(action_dim, config.n_embd, hidden_dims=[256])
 
         # embedding dropout
@@ -533,7 +537,7 @@ class ExplicitSAHNGPT(nn.Module):
 
         return r, v_r_norm
 
-    def forward(self, states, timesteps, actions=None, keys=None):
+    def forward(self, states, timesteps, actions=None, keys=None, future_states=None):
         B, T = states.shape[0], states.shape[1]
         states.requires_grad = True
 
@@ -548,7 +552,12 @@ class ExplicitSAHNGPT(nn.Module):
         token_embeddings[:, 0:(T * 3):3, :] = state_embeddings
         if keys is not None:
             # keys should not be None (???????)
-            grad_embeddings = self.state_grad_encoder(states_grad)
+            if self.use_future_state:
+                assert future_states is not None
+                goal_vecs = torch.cat([states_grad, future_states], dim=-1)
+            else:
+                goal_vecs = states_grad
+            grad_embeddings = self.state_grad_encoder(goal_vecs)
             token_embeddings[:, 1:(T * 3):3, :] = grad_embeddings
 
         if actions is not None:
@@ -573,9 +582,6 @@ class ExplicitSAHNGPT(nn.Module):
         action_preds = self.action_predictor(x_action[:, 1:(T * 3):3, :])
         # Use it as ActNet, do action prediction
         return action_preds, r, v_r_norm
-
-
-
 
 
 class ENet(nn.Module):

src/module/VQ.py
@@ -19,7 +19,7 @@ from module_util import MLP, TimeSphereEncoder
 #     ut = torch.div(ut_cumsum, ut_cumsum[-1, 0] + 1e-12)
 #     return ut
 
-class increaseEncourageIdentityMap(torch.autograd.Function):
+class increaseEncourageIdentityMap(Function):
     @staticmethod
     def forward(ctx, input):
         ctx.save_for_backward(input)
@@ -36,7 +36,7 @@ increae_encourage_identity_map = increaseEncourageIdentityMap.apply
 
 class VQClassifierNNTime(nn.Module):
     def __init__(self, key_dim, n_e, e_dim, e_split, KT=0.1, use_ema=False, coe_ema=0.5, t_emb_rate=1.2,
-                 use_clip_decrease_r=False):
+                 use_prob_sel_train=False, use_timestep_appeal=False):
         super().__init__()
         self.key_dim = key_dim
         self.n_e = n_e
@@ -53,10 +53,6 @@ class VQClassifierNNTime(nn.Module):
         self.r_keys = nn.Embedding(n_e, 1)
         nn.init.constant_(self.r_keys.weight, 0.999)
         self.r_activate = nn.Hardtanh(min_val=0.0, max_val=1.0)
-        self.r_p_reset = 1.0 / n_e
-
-        self.prev_r_keys = nn.Embedding(n_e, 1)  # when training, log the previous r_keys
-        nn.init.constant_(self.prev_r_keys.weight, 1.0)
 
         arange = torch.arange(n_e, dtype=torch.float32)
         # along with scaler that transform them into different time area
@@ -73,18 +69,23 @@ class VQClassifierNNTime(nn.Module):
         print('init ut', ut.unsqueeze(-1))
         self.t_keys.weight.data = ut
 
+        self.use_prob_sel_train = use_prob_sel_train
+        self.use_timestep_appeal = use_timestep_appeal
+
         # parameters
         self.vparams = nn.Embedding(n_e, e_dim)
         self.vparams.weight.data.uniform_(-1.0 / n_e, 1.0 / n_e)
 
         self.use_ema = use_ema
         self.coe_ema = coe_ema
-
-        self.use_clip_decrease_r = use_clip_decrease_r
         # None or a float point bound (how to calculate it will be defined in autocot model...)
 
         self.register_buffer('arange', arange)
         self.register_buffer('not_same', 1.0 - torch.eye(n_e))
+        mask_adj = torch.abs(torch.sub(self.arange.view(-1, 1), self.arange))
+        mask_adj = torch.le(mask_adj, 1)
+        print('mask_adj\n', mask_adj)
+        self.register_buffer('mask_adj', mask_adj)
 
     def get_keys(self):
         # for the reason that our keys and its time embedding is separated
@@ -98,33 +99,12 @@ class VQClassifierNNTime(nn.Module):
         r = self.r_activate(self.r_keys.weight)
         return r  # (n_e, 1)
 
-    def log_r(self):
-        # log the (PREVIOUS) r-s, BE CAREFUL when using it
-        self.prev_r_keys.weight.data = self.get_r()
-
     def reset_oor_r(self, left_step=10_000):
         with torch.no_grad():
             # First deal with the clipping
-            if self.use_clip_decrease_r:
-                key_decrease_bound = self.prev_r_keys.weight.data * (1.0 - 1.0 / left_step)
-                m_decrease = torch.greater(key_decrease_bound, self.r_keys.weight.data)
-                self.r_keys.weight.data = torch.where(m_decrease, key_decrease_bound, self.r_keys.weight.data)
-
             # then dealing with value out of range [0.001, 0.999]
             m_999 = torch.greater(self.r_keys.weight.data, 0.999)
             m_1 = torch.less(self.r_keys.weight.data, 0.1)
-
-            # add the random reset part
-            # r_minus = torch.cat([self.r_keys.weight.data[:-1, :],
-            #                      torch.zeros(size=(1, 1), device=self.r_keys.weight.data.device)], dim=0)
-            # r_plus = torch.cat([torch.zeros(size=(1, 1), device=self.r_keys.weight.data.device),
-            #                     self.r_keys.weight.data[1:, :]], dim=0)
-            # w = torch.cat([r_minus, self.r_keys.weight.data, r_plus], dim=1)
-            # w = torch.max(w, dim=1)
-            # m_rand = torch.rand_like(self.r_keys.weight.data)
-            # m_rand = torch.less(m_rand, w * self.r_p_reset)
-            # m_rand = torch.logical_and(m_rand, m_001)  # choose the rand reset one in m_001
-            # m_999 = torch.logical_or(m_999, m_rand)  # they will be reset!
 
             self.r_keys.weight.data = torch.where(m_1, 0.1, self.r_keys.weight.data)
             self.r_keys.weight.data = torch.where(m_999, 0.999, self.r_keys.weight.data)
@@ -162,7 +142,7 @@ class VQClassifierNNTime(nn.Module):
             B, T = key_soft.shape[0], key_soft.shape[1]
             key_soft_norm = F.normalize(key_soft, p=2.0, dim=-1)
             key_soft_t_emb = self.t_emb(feature=key_soft_norm, unified_t=u_t)  # key_soft: (B, T, key_dim + 1)
-            key_soft_flattened = key_soft_t_emb.view(B*T, self.key_dim + 1)  # (B*T, key_dim + 1)
+            key_soft_flattened = key_soft_t_emb.view(B*T, self.key_dim+1)  # (B*T, key_dim + 1)
 
             # construct usage keys in the codebook (time + feature)
             keys_norm = self.get_keys()  # (n_e, key_dim + 1)
@@ -175,6 +155,18 @@ class VQClassifierNNTime(nn.Module):
 
             return encoding_indices
 
+    def get_w_cnt(self, encoding_indices):
+        B, T = encoding_indices.shape
+        ei_flattened = encoding_indices.view(-1)
+        sum_mask = torch.eq(self.arange.view(-1, 1).repeat(1, B*T),
+                            ei_flattened.view(1, -1).repeat(self.n_e, 1))
+        # print('sum_mask.shape', sum_mask.shape)
+        cnt = torch.add(torch.sum(sum_mask, dim=-1), 1.0)
+        w = torch.div(1.0, cnt)
+        w = torch.nan_to_num(w)
+        w_cnt = w[encoding_indices]
+        return w_cnt
+
     def ema_forward(self, key_soft, u_t):
         # key_soft shape (B, T, self.key_dim)
         key_soft = key_soft.contiguous()
@@ -183,11 +175,10 @@ class VQClassifierNNTime(nn.Module):
         # first construct key_soft with timestep embedding
         key_soft_norm = F.normalize(key_soft, p=2.0, dim=-1)  # (B, T, key_dim)
         key_soft_t_emb = self.t_emb(feature=key_soft_norm, unified_t=u_t)  # (B, T, key_dim + 1)
-        key_soft_flattened = key_soft_t_emb.view(B*T, self.key_dim + 1)  # (B*T, key_dim + 1)
+        key_soft_flattened = key_soft_t_emb.view(B*T, self.key_dim+1)  # (B*T, key_dim + 1)
 
         # construct usage keys in the codebook (time + feature)
         keys_norm = self.get_keys()  # (n_e, key_dim + 1)
-        self.log_r()  # save current rs
         # print('keys_norm.shape:', keys_norm.shape)
         keys_norm = keys_norm.detach()  # update of ema do not need gradient
         keys_norm = keys_norm * self.get_r()  # (n_e, key_dim + 1)
@@ -204,15 +195,19 @@ class VQClassifierNNTime(nn.Module):
         # EMA update of keys behind
         with torch.no_grad():
             u_t_flattened = u_t.view(-1)  # (B*T)
-            time_low_mask = \
-                torch.greater_equal(u_t_flattened.view(1, -1).repeat(self.n_e, 1), self.t_base)
-            time_high_mask = \
-                torch.less(u_t_flattened.view(1, -1).repeat(self.n_e, 1), torch.add(self.t_base, 1.0 / self.n_e))
-            time_mask = torch.logical_and(time_low_mask, time_high_mask)  # (n_e, B*T)
+
 
             sum_mask = torch.eq(self.arange.view(-1, 1).repeat(1, B * T),
                                 encoding_indices_flattened.view(1, -1).repeat(self.n_e, 1))
-            sum_mask = torch.logical_or(sum_mask, time_mask)
+            if self.use_timestep_appeal:
+                print('use_timestep_appeal')
+                time_low_mask = \
+                    torch.greater_equal(u_t_flattened.view(1, -1).repeat(self.n_e, 1), self.t_base)
+                time_high_mask = \
+                    torch.less(u_t_flattened.view(1, -1).repeat(self.n_e, 1), torch.add(self.t_base, 1.0 / self.n_e))
+                time_mask = torch.logical_and(time_low_mask, time_high_mask)  # (n_e, B*T)
+                sum_mask = torch.logical_or(sum_mask, time_mask)
+
             sum_mask = sum_mask.to(dtype=torch.float32)
 
             cnt = torch.sum(sum_mask, dim=-1, keepdim=True)  # (n_e, 1)
@@ -243,7 +238,7 @@ class VQClassifierNNTime(nn.Module):
             # print('max min', max(encoding_indices_flattened), min(encoding_indices_flattened))
             # input()
             encoding_indices = encoding_indices_flattened.view(B, T)  # (B, T)
-
+            w_cnt = self.get_w_cnt(encoding_indices)
 
         # re-calculate score_ksh...
         # calc score_ksh
@@ -252,13 +247,28 @@ class VQClassifierNNTime(nn.Module):
         keys_norm = keys_norm * increae_encourage_identity_map(self.get_r())  # (n_e, key_dim + 1)
         score_ksh_flattened = torch.einsum('bd,dn->bn', key_soft_flattened,
                                            rearrange(keys_norm, 'n d -> d n'))  # (B*T, n_e)
-        score_ksh = score_ksh_flattened.view(B, T, self.n_e)  # (B, T, n_e)
+        # score_ksh = score_ksh_flattened.view(B, T, self.n_e)  # (B, T, n_e)
         # transmit into weight
         w_flattened = self.score_to_weight(score_ksh_flattened)  # (B*T, n_e)
 
         # choose out prototype prob for each soft key (after update...)
+        # if not self.use_prob_sel_train:
+        #     w_max_flattened = torch.gather(w_flattened, dim=-1, index=encoding_indices_flattened.unsqueeze(-1)).squeeze(-1)
+        #     w_max = w_max_flattened.view(B, T)
+        if self.use_prob_sel_train:
+            print('use_prob_sel_train')
+            # we only choose the adjacent index
+            # print('use_prob_sel_train')
+            mask_adj = self.mask_adj[encoding_indices_flattened]
+            score_ksh_flattened_mask = torch.where(mask_adj, score_ksh_flattened, float('-inf'))
+            w_flattened_mask = self.score_to_weight(score_ksh_flattened_mask)
+            # reset encoding_indices_flattened using prob sampling
+            encoding_indices_flattened = w_flattened_mask.view(-1, self.n_e).multinomial(1).view(B*T)
+            encoding_indices = encoding_indices_flattened.view(B, T)
+
         w_max_flattened = torch.gather(w_flattened, dim=-1, index=encoding_indices_flattened.unsqueeze(-1)).squeeze(-1)
         w_max = w_max_flattened.view(B, T)
+
         # print(w_max.shape)
 
         # to weighted params
@@ -290,7 +300,7 @@ class VQClassifierNNTime(nn.Module):
 
         return \
             encoding_indices, key_hard, \
-            vparams_w, vparams_hard, w_max, score_ksh, key_soft_t_emb
+            vparams_w, vparams_hard, w_max, w_cnt, key_soft_t_emb
 
     def forward(self, key_soft, u_t):
         # key_soft shape (B, T, self.key_dim)